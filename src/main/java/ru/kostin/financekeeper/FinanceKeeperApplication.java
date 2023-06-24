11923b2ea060fcc6fc904f896a662b138b2bf6b5
Sergey Kostin <serg89981@icloud.com>
2023-06-24 21:57:19 +0300

    init

---
+++ src/main/java/ru/kostin/financekeeper/FinanceKeeperApplication.java
@@ -0,0 +1,13 @@
+package ru.kostin.financekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceKeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kostin/financekeeper/FinanceKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kostin.financekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
