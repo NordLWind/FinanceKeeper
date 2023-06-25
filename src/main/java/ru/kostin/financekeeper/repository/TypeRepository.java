cb1269d608765132bbb347bca61f69a3bc1ad369
Sergey Kostin <serg89981@icloud.com>
2023-06-25 15:47:55 +0300

    fix type updating method

--- src/main/java/ru/kostin/financekeeper/repository/TypeRepository.java
+++ src/main/java/ru/kostin/financekeeper/repository/TypeRepository.java
@@ -1,11 +1,15 @@
 package ru.kostin.financekeeper.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kostin.financekeeper.entity.Type;
 
 public interface TypeRepository extends JpaRepository<Type, Long> {
-
-    void updateTypeById(String val, long id);
-
+    @Transactional
+    @Modifying
+    @Query("update Type t set t.type = ?1 where t.id = ?2")
+    void updateTypeById(String type, Long id);
     boolean existsByType(String type);
 }
