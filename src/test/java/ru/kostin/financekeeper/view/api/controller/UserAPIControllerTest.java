727eb8175ed7536b6b5f05eea850216217d189fa
Sergey Kostin <serg89981@icloud.com>
2023-06-27 12:43:52 +0300

    add UserControllerTest

---
+++ src/test/java/ru/kostin/financekeeper/view/api/controller/UserAPIControllerTest.java
@@ -0,0 +1,55 @@
+package ru.kostin.financekeeper.view.api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.kostin.financekeeper.security.MockSecurityConfiguration;
+import ru.kostin.financekeeper.security.SecurityConfiguration;
+import ru.kostin.financekeeper.service.UserService;
+import ru.kostin.financekeeper.view.api.json.CompletionResponse;
+import ru.kostin.financekeeper.view.api.json.user.AddUserRequest;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserAPIController.class)
+@Import({SecurityConfiguration.class, MockSecurityConfiguration.class})
+@RunWith(SpringRunner.class)
+class UserAPIControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper om;
+
+    @MockBean
+    UserService userService;
+
+    @Test
+    void add() throws Exception {
+        doNothing().when(userService).add("test", "test@t", "password");
+
+        AddUserRequest request = new AddUserRequest();
+        request.setName("test");
+        request.setEmail("test@t");
+        request.setPassword("password");
+
+        mockMvc.perform(post("/api/user/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(om.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(om.writeValueAsString(new CompletionResponse(true))));
+        verify(userService).add("test", "test@t", "password");
+    }
+}
