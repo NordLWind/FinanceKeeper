45844557ea2f4d28fa0bcd0b9af91f02e34c0895
Sergey Kostin <serg89981@icloud.com>
2023-06-28 15:47:03 +0300

    removed authManager

--- src/main/java/ru/kostin/financekeeper/view/api/controller/UserAPIController.java
+++ src/main/java/ru/kostin/financekeeper/view/api/controller/UserAPIController.java
@@ -3,11 +3,6 @@ package ru.kostin.financekeeper.view.api.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +11,6 @@ import ru.kostin.financekeeper.exception.ItemAlreadyExistsException;
 import ru.kostin.financekeeper.service.UserService;
 import ru.kostin.financekeeper.view.api.json.CompletionResponse;
 import ru.kostin.financekeeper.view.api.json.user.AddUserRequest;
-import ru.kostin.financekeeper.view.api.json.user.AuthRequest;
 
 import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.http.ResponseEntity.status;
@@ -25,20 +19,7 @@ import static org.springframework.http.ResponseEntity.status;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserAPIController extends AbstractAPIController {
-    private final AuthenticationManager authenticationManager;
     private final UserService userService;
-
-    @PostMapping("/login")
-    public ResponseEntity<CompletionResponse> authenticateUser(@RequestBody AuthRequest authRequest) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            return ok(new CompletionResponse(true));
-        } catch (AuthenticationException e) {
-            return status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-    }
 
     @PostMapping("/register")
     public ResponseEntity<CompletionResponse> add(@RequestBody AddUserRequest req) {
