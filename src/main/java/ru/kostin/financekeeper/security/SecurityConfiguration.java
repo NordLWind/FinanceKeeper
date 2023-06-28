99ab5c5c3239a1a0bc745b1976b64bd389477c4e
Sergey Kostin <serg89981@icloud.com>
2023-06-28 15:47:03 +0300

    removed authManager

--- src/main/java/ru/kostin/financekeeper/security/SecurityConfiguration.java
+++ src/main/java/ru/kostin/financekeeper/security/SecurityConfiguration.java
@@ -1,29 +1,18 @@
 package ru.kostin.financekeeper.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableMethodSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public AuthenticationManager getAuthenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/login").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/user/*").permitAll()
+                .antMatchers("/api/user/*").permitAll()
                 .antMatchers("/api/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name());
 
         http.formLogin()
--- src/main/java/ru/kostin/financekeeper/view/api/controller/UserAPIController.java
+++ src/main/java/ru/kostin/financekeeper/view/api/controller/UserAPIController.java
@@ -3,11 +3,6 @@ package ru.kostin.financekeeper.view.api.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +13,9 @@ import ru.kostin.financekeeper.view.api.json.CompletionResponse;
 import ru.kostin.financekeeper.view.api.json.user.AddUserRequest;
 import ru.kostin.financekeeper.view.api.json.user.AuthRequest;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.http.ResponseEntity.status;
 
@@ -25,19 +23,16 @@ import static org.springframework.http.ResponseEntity.status;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserAPIController extends AbstractAPIController {
-    private final AuthenticationManager authenticationManager;
     private final UserService userService;
 
-    @PostMapping("/login")
-    public ResponseEntity<CompletionResponse> authenticateUser(@RequestBody AuthRequest authRequest) {
+    @PostMapping("/auth")
+    public ResponseEntity<CompletionResponse> auth(AuthRequest authRequest, HttpServletRequest servletRequest) {
         try {
-            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            servletRequest.login(authRequest.getEmail(), authRequest.getPassword());
             return ok(new CompletionResponse(true));
-        } catch (AuthenticationException e) {
+        } catch (ServletException e) {
             return status(HttpStatus.UNAUTHORIZED).build();
         }
-
     }
 
     @PostMapping("/register")
