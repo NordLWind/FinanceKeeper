5bf4e9ba50c689c719586a53c88c560eb0f41dec
Sergey Kostin <serg89981@icloud.com>
2023-06-28 20:09:01 +0300

    removed authManager

--- src/main/java/ru/kostin/financekeeper/view/api/controller/UserAPIController.java
+++ src/main/java/ru/kostin/financekeeper/view/api/controller/UserAPIController.java
@@ -11,10 +11,6 @@ import ru.kostin.financekeeper.exception.ItemAlreadyExistsException;
 import ru.kostin.financekeeper.service.UserService;
 import ru.kostin.financekeeper.view.api.json.CompletionResponse;
 import ru.kostin.financekeeper.view.api.json.user.AddUserRequest;
-import ru.kostin.financekeeper.view.api.json.user.AuthRequest;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.http.ResponseEntity.status;
@@ -24,16 +20,6 @@ import static org.springframework.http.ResponseEntity.status;
 @RequiredArgsConstructor
 public class UserAPIController extends AbstractAPIController {
     private final UserService userService;
-
-    @PostMapping("/auth")
-    public ResponseEntity<CompletionResponse> auth(AuthRequest authRequest, HttpServletRequest servletRequest) {
-        try {
-            servletRequest.login(authRequest.getEmail(), authRequest.getPassword());
-            return ok(new CompletionResponse(true));
-        } catch (ServletException e) {
-            return status(HttpStatus.UNAUTHORIZED).build();
-        }
-    }
 
     @PostMapping("/register")
     public ResponseEntity<CompletionResponse> add(@RequestBody AddUserRequest req) {
